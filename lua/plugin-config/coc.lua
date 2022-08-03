8ff393c12076f09b4c8e64bb702507929e4848d3
HaomingJu <juhaoming@trunk.tech>
2022-08-03 16:13:17 +0800

    Fix for coc.nvim v0.0.82

--- lua/plugin-config/coc.lua
+++ lua/plugin-config/coc.lua
@@ -1,45 +1,7 @@
 vim.cmd([[
 
-let g:coc_config_home = '~/.config/nvim/lua/plugin-config'
-
-"Coc 插件
-call coc#add_extension('coc-json', 'coc-cmake', 'coc-snippets', 'coc-pairs', 'coc-pyright', 'coc-vimlsp', 'coc-docker', 'coc-ultisnips', 'coc-clangd', 'coc-sumneko-lua', 'coc-clang-format-style-options')
-
-" Coc补全, Tab触发
-function! s:check_back_space() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~ '\s'
-endfunction
-
-inoremap <silent><expr> <Tab>
-      \ pumvisible() ? "\<C-n>" :
-      \ <SID>check_back_space() ? "\<Tab>" :
-      \ coc#refresh()
-
-inoremap <expr> <c-j> pumvisible() ? "\<C-n>" : coc#refresh()
-inoremap <expr> <c-k> pumvisible() ? "\<C-p>" : coc#refresh()
-
-" 取消<C-e>原有功能, 将其置为expand展开功能
-map <C-e> <NOP>
-inoremap <silent><expr> <C-e>
-      \ pumvisible() ? coc#_select_confirm() :
-      \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
-      \ <SID>check_back_space() ? "\<TAB>" :
-      \ coc#refresh()
-
-let g:coc_snippet_next = '<tab>'
-
-inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
-				\: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-
-" 跳转至定义处
-nmap <Leader>d <Plug>(coc-definition)
-
-
-"函数签名提示
-autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-
-nnoremap <silent> K :call <SID>show_documentation()<CR>
+    let g:coc_config_home = '~/.config/nvim/lua/plugin-config'
+    let g:coc_global_extensions = ['coc-json', 'coc-cmake', 'coc-snippets', 'coc-pairs', 'coc-pyright', 'coc-vimlsp', 'coc-docker', 'coc-ultisnips', 'coc-clangd', 'coc-sumneko-lua', 'coc-clang-format-style-options']
 
 function! s:show_documentation()
   if (index(['vim','help'], &filetype) >= 0)
@@ -49,9 +11,31 @@ function! s:show_documentation()
   endif
 endfunction
 
-set updatetime=300
-set shortmess+=c
+" Show documentation for the current word
+nnoremap <silent> K :call <SID>show_documentation()<CR>
 
+" jump to the definition of the current word
+nmap <Leader>d <Plug>(coc-definition)                           
+
+" show signature help for the current word
+autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp') 
+
+" auto highlight the current word
 autocmd CursorHold * silent call CocActionAsync('highlight')
 
+inoremap <expr> <c-j> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+inoremap <expr> <c-k> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+inoremap <expr> <cr> coc#pum#visible() ? coc#_select_confirm() : "\<CR>"
+
+" Let tab can jump outside of ">" or ")" on insert mode
+let g:coc_snippet_next = '<tab>'
+
+
+" https://www.ditig.com/256-colors-cheat-sheet
+hi CocSearch    ctermfg=None    guibg=None
+hi CocMenuSel   ctermbg=34      guibg=#00af00
+
+set updatetime=300
+set shortmess+=c
 ]])
+
